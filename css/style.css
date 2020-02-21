3765a4adf5398915642dd9fddb9e2243bd9f339b
MrKouam <flashscoup@gmail.com>
2020-02-21 16:38:15 +0000

    Exo de cour sur git

---
+++ css/style.css
@@ -0,0 +1,50 @@
+body {
+  margin-top: 105px; }
+
+.navbar {
+  box-shadow: 2px 2px #3292a6;
+  opacity: 0.9;
+  background-color: #fff; }
+  .navbar .nav-item {
+    font-size: 1.4rem;
+    padding-right: 20px;
+    font-weight: 600; }
+
+#showcase {
+  position: relative;
+  background-image: url('../images/mountains.jpg');
+  min-height: 600px; }
+  #showcase .overlay {
+    background-color: rgba(50, 146, 166, 0.8);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%; }
+#about{
+}
+.info-header{
+    text-align: center;
+    border-bottom:#ddd 2px solid;
+    width:50%;
+    margin: auto;
+}
+#authors img{
+    margin-top: -50px;
+
+}
+#authors .fa{
+    font-size: 25px;
+    
+}
+#authors .card:hover{
+    background-color: #3292a6;
+    color:#fff;
+}
+#authors .card .fa:hover{
+    color:#fff;
+}
+#contact{
+    
+}
+/*# sourceMappingURL=style.css.map */
